4745de2cacad6ccd735af9fd7a704256afe1834d
Marcel Canhisares <marcel.canhisares@gmail.com>
2019-12-26 18:43:58 -0300

    Updating library version and adding set subject methods

--- android/src/main/java/com/snowplowanalytics/react/tracker/RNSnowplowTrackerModule.java
+++ android/src/main/java/com/snowplowanalytics/react/tracker/RNSnowplowTrackerModule.java
@@ -10,12 +10,15 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.snowplowanalytics.react.util.EventUtil;
 import com.snowplowanalytics.snowplow.tracker.Emitter;
+import com.snowplowanalytics.snowplow.tracker.Subject;
 import com.snowplowanalytics.snowplow.tracker.Tracker;
 import com.snowplowanalytics.snowplow.tracker.emitter.HttpMethod;
 import com.snowplowanalytics.snowplow.tracker.emitter.RequestSecurity;
 import com.snowplowanalytics.snowplow.tracker.events.SelfDescribing;
 import com.snowplowanalytics.snowplow.tracker.events.Structured;
 import com.snowplowanalytics.snowplow.tracker.events.ScreenView;
+
+import javax.annotation.Nullable;
 
 public class RNSnowplowTrackerModule extends ReactContextBaseJavaModule {
 
@@ -35,7 +38,8 @@ public class RNSnowplowTrackerModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void initialize(String endpoint, String method, String protocol,
-                           String namespace, String appId, ReadableMap options) {
+                           String namespace, String appId, boolean enableGeoLocation,
+                           ReadableMap options) {
         this.emitter = new Emitter.EmitterBuilder(endpoint, this.reactContext)
                 .method(method.equalsIgnoreCase("post") ? HttpMethod.POST : HttpMethod.GET)
                 .security(protocol.equalsIgnoreCase("https") ? RequestSecurity.HTTPS : RequestSecurity.HTTP)
@@ -45,9 +49,62 @@ public class RNSnowplowTrackerModule extends ReactContextBaseJavaModule {
                 .TrackerBuilder(this.emitter, namespace, appId, this.reactContext)
                 .base64(false)
                 .mobileContext(true)
+                .geoLocationContext(enableGeoLocation)
                 .screenviewEvents(options.hasKey("autoScreenView") ? options.getBoolean("autoScreenView") : false)
                 .build()
         );
+        Subject subject = new Subject.SubjectBuilder().build();
+        this.tracker.setSubject(subject);
+    }
+
+    @ReactMethod
+    public void setSubjectUserId(String userId) {
+        tracker.getSubject().setUserId(userId);
+    }
+
+    @ReactMethod
+    public void setSubjectColorDepth(Integer colorDepth) {
+        tracker.getSubject().setColorDepth(colorDepth);
+    }
+
+    @ReactMethod
+    public void useSubjectDefaultScreenResolution() {
+        tracker.getSubject().setDefaultScreenResolution(reactContext);
+    }
+
+    @ReactMethod
+    public void setSubjectIpAddress(String ipAddress) {
+        tracker.getSubject().setIpAddress(ipAddress);
+    }
+
+    @ReactMethod
+    public void setSubjectLanguage(String language) {
+        tracker.getSubject().setLanguage(language);
+    }
+
+    @ReactMethod
+    public void setSubjectNetworkUserId(String networkUserId) {
+        tracker.getSubject().setNetworkUserId(networkUserId);
+    }
+
+    @ReactMethod
+    public void setSubjectUserId(Integer width, Integer height) {
+        tracker.getSubject().setScreenResolution(width, height);
+    }
+
+    @ReactMethod
+    public void setSubjectTimezone(String timezone) {
+        tracker.getSubject().setTimezone(timezone);
+    }
+
+    @ReactMethod
+    public void setSubjectUseragent(String useragent) {
+        tracker.getSubject().setUseragent(useragent);
+    }
+
+    @ReactMethod
+    public void setSubjectViewPort(Integer width, Integer height) {
+        tracker.getSubject().setViewPort(width, height);
     }
 
     @ReactMethod
